19c64d9805c02a0d4bdc581d56329a0a08414627
j-alencar <joao.alencar@univille.br>
2026-02-20 21:16:47 -0300

    cross-distro

---
+++ dot_config/firefox/user.js
@@ -0,0 +1,9 @@
+user_pref("sidebar.revamp", false);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("font.name.monospace.x-western", "JetBrains Mono");
+user_pref("font.name.sans-serif.x-western", "JetBrains Mono");
+user_pref("font.name.serif.x-western", "JetBrains Mono");
+user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
+user_pref("browser.theme.content-theme", 0);
+user_pref("browser.theme.toolbar-theme", 0);
+user_pref("ui.systemUsesDarkTheme", 1);
