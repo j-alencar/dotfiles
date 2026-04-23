5c531690385e822d63baad204f1bc38a1dfa3b25
João Alencar <j-alencar.univille.br>
2026-04-23 00:07:30 -0300

    ih8jspacmans

--- dot_config/zsh/nvm.zsh
+++ dot_config/zsh/nvm.zsh
@@ -9,3 +9,16 @@ nvm() { _nvm_load && nvm "$@" }
 node() { _nvm_load && node "$@" }
 npm() { _nvm_load && npm "$@" }
 npx() { _nvm_load && npx "$@" }
+
+# Eagerly add default nvm node bin to PATH so nvm-installed tools
+# are found before Windows npm PATH entries
+_nvm_default_bin="$NVM_DIR/alias/default"
+if [[ -f "$_nvm_default_bin" ]]; then
+    _nvm_default_version=$(cat "$_nvm_default_bin")
+    while [[ -f "$NVM_DIR/alias/$_nvm_default_version" ]]; do
+        _nvm_default_version=$(cat "$NVM_DIR/alias/$_nvm_default_version")
+    done
+    _nvm_bin="$NVM_DIR/versions/node/$_nvm_default_version/bin"
+    [[ -d "$_nvm_bin" ]] && path=("$_nvm_bin" $path)
+fi
+unset _nvm_default_bin _nvm_default_version _nvm_bin
